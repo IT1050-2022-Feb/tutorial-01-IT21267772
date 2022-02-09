e4f0f284ab883881a8371e906985d53e8be7ed84
IT21267772 <95214934+IT21267772@users.noreply.github.com>
2022-02-09 18:09:38 +0000

    Exercise 3 completed

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,19 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+
+  int n, sum = 0;
+
+  printf("Enter n : ");
+  scanf("%d", &n);
+
+  while(n > 0)
+  {
+    sum += n;
+    n--;
+  }
+
+  printf("Sum : %d\n", sum);
   
   return 0;
 }
